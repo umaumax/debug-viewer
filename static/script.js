5c0b8ba62d4264b8ca51efebc00ab4a4185a3bd8
umaumax <umauma7083@gmail.com>
2023-09-24 00:04:46 +0900

    Refactor

--- static/script.js
+++ static/script.js
@@ -28,31 +28,11 @@ window.addEventListener('resize', () => {
     update_canvas()
 });
 
-// // ジオメトリとマテリアルの作成
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({
-// color: 0x00ff00
-// });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-//
-// // アニメーションの更新
-// const animate = () => {
-// requestAnimationFrame(animate);
-//
-// // オブジェクトを回転させるアニメーション
-// cube.rotation.x += 0.01;
-// cube.rotation.y += 0.01;
-//
-// renderer.render(scene, camera);
-// };
-
 class ObjectListMap {
     constructor() {
         this.map = new Map();
     }
 
-    // キーに対してオブジェクトを追加
     addObjectWithLabel(object, label) {
         if (!this.map.has(label)) {
             this.map.set(label, []);
@@ -60,12 +40,10 @@ class ObjectListMap {
         this.map.get(label).push(object);
     }
 
-    // キーを指定してオブジェクトの配列を取得
     getObjectsByLabel(label) {
         return this.map.get(label) || [];
     }
 
-    // キーを指定してオブジェクトの存在を判定
     hasObjectWithLabel(label) {
         return this.map.has(label);
     }
@@ -97,20 +75,11 @@ function ToVertices(geometry) {
  * @param {THREE.Scene} scene - The Three.js scene.
  */
 function addPointToLine(line, newX, newY, newZ, material, scene) {
-    // Get current vertices from the geometry
-    // const currentVertices = line.geometry.attributes.position.array;
-    // const newCurrentVertices = new Float32Array(currentVertices.length + 3)
-    // newCurrentVertices.set([...currentVertices, newX, newY, newZ]);
-
     // Create a new geometry with the updated vertices
     var points = ToVertices(line.geometry)
     points.push(new THREE.Vector3(newX, newY, newZ))
-    // for (let i = 0; i < newCurrentVertices.length; i += 3) {
-    // points.push(new THREE.Vector3(newCurrentVertices[i], newCurrentVertices[i + 1], newCurrentVertices[i + 2]));
-    // }
     const newGeometry = new THREE.BufferGeometry().setFromPoints(points);
 
-    // Create or update the Line object
     const newLine = new THREE.Line(newGeometry, material);
 
     // Remove the old Line object from the scene and add the new one
@@ -120,45 +89,22 @@ function addPointToLine(line, newX, newY, newZ, material, scene) {
     return newLine;
 }
 
-// Example usage:
-// addPointToLine(line, 6, 2, 0, material, scene);
-
-// // 連続した点の座標を設定
-// const points = [];
-// for (let i = 0; i < 100; i++) {
-// const x = (i - 50) * 0.1;
-// const y = Math.sin(x); // 任意の点のy座標を設定
-// const z = 0;
-// points.push(new THREE.Vector3(x, y, z));
-// }
-//
-// // ラインのマテリアルを作成
-// const material = new THREE.LineBasicMaterial({
-// color: 0x00aa00
-// });
-//
-// // 連続した線を作成
-// const geometry = new THREE.BufferGeometry().setFromPoints(points);
-// const line = new THREE.Line(geometry, material);
-//
-// // シーンに追加
-// scene.add(line);
-
 class CustomLine {
     constructor(scene, material) {
         this.scene = scene;
-        this.points = [] // this.generatePoints();
+        this.points = []
         this.geometry = new THREE.BufferGeometry().setFromPoints(this.points);
         this.material = material
         this.line = new THREE.Line(this.geometry, material);
         this.addToScene();
     }
 
+    // for debug
     generatePoints() {
         const points = [];
         for (let i = 0; i < 100; i++) {
             const x = (i - 50) * 0.1;
-            const y = Math.sin(x); // 任意の点のy座標を設定
+            const y = Math.sin(x);
             const z = 0;
             points.push(new THREE.Vector3(x, y, z));
         }
@@ -181,44 +127,23 @@ const line = new CustomLine(scene, new THREE.LineBasicMaterial({
     color: 0x005500
 }))
 
-// const customLine = new CustomLine(scene);
-
-// const emphasizedPointIndex = 50; // 例: 50番目の点を強調表示
-
-// function emphasizePoint(index) {
-// const emphasizedPoint = points[index];
-//
-// const sphereGeometry = new THREE.SphereGeometry(0.02, 32, 32);
-// const sphereMaterial = new THREE.MeshBasicMaterial({
-// color: 0x00ff00
-// });
-// const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-// sphere.position.copy(emphasizedPoint);
-//
-// scene.add(sphere);
-// }
-//
-// for (let i = 0; i < points.length; i++) {
-// emphasizePoint(i);
-// }
-
 class CustomSphere {
     constructor(scene, material) {
         this.scene = scene;
         this.spheres = [];
         this.material = material
-        // this.generateSpheres();
         this.addToScene();
     }
 
+    // for debug
     generateSpheres() {
         for (let i = 0; i < 100; i++) {
             const x = (i - 50) * 0.1;
-            const y = Math.sin(x); // 任意の点のy座標を設定
+            const y = Math.sin(x);
             const z = 0;
-            const sphereGeometry = new THREE.SphereGeometry(0.02, 16, 16); // 球体のジオメトリを作成
-            const sphere = new THREE.Mesh(sphereGeometry, this.material); // 球体を作成
-            sphere.position.set(x, y, z); // 球体の位置を設定
+            const sphereGeometry = new THREE.SphereGeometry(0.02, 16, 16);
+            const sphere = new THREE.Mesh(sphereGeometry, this.material);
+            sphere.position.set(x, y, z);
             this.spheres.push(sphere);
         }
     }
@@ -306,26 +231,6 @@ function set_axis(scene) {
 }
 set_axis(scene)
 
-// var t = 0;
-//
-// function animate_arrow() {
-// var index = parseInt(t / 5) % points.length
-// const point = points[index];
-// var pos = point
-//
-// // 矢印の位置と回転を定期的に更新
-// const time = t * 0.01; // 時間に基づいて位置と回転を更新する例
-// // const newX = Math.sin(time);
-// // const newY = Math.cos(time);
-// const newRotation = new THREE.Euler(0, time, 0);
-// // new THREE.Euler().setFromQuaternion( arrow.quaternion );
-// t += 1;
-// customArrow.position.copy(pos);
-// // customArrow.position.set(newX, newY, 0);
-// customArrow.rotation.copy(newRotation);
-// customArrow.update()
-// }
-
 const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
@@ -392,17 +297,6 @@ function init_mouse_control(canvas) {
         event.preventDefault();
         event.stopPropagation();
     });
-
-    // 移動
-    // const zoomSpeed = 0.1;
-    //
-    // if (event.deltaY < 0) {
-    // // ホイールを上にスクロールした場合、ズームイン
-    // camera.position.z -= zoomSpeed;
-    // } else {
-    // // ホイールを下にスクロールした場合、ズームアウト
-    // camera.position.z += zoomSpeed;
-    // }
 }
 init_mouse_control(canvas)
 
@@ -412,13 +306,9 @@ const port = 8765
 // const path = 'ws/get/dummy'
 const path = 'ws/get/database'
 const url = 'ws://' + host + ':' + port + '/' + path
-// const url = 'http://' + host + ':' + port + '/' + path
-console.log(url)
 const socket = new WebSocket(url);
-// const socket = io(url);
 
 socket.addEventListener('open', (event) => {
-    // socket.on('connection', (socket) => {
     console.log('connection opened');
 
     const query_data = {
@@ -430,7 +320,6 @@ socket.addEventListener('open', (event) => {
 });
 
 socket.addEventListener('message', (event) => {
-    // socket.on('message', (event) => {
     const data = JSON.parse(event.data);
     console.log('received data:', data);
 
@@ -451,10 +340,10 @@ socket.addEventListener('message', (event) => {
     timestampInput.value = timestamp;
 });
 
-// socket.addEventListener('close', (event) => {
-// console.log(`connection closed code: ${event.code} reason: ${event.reason}`);
-// });
+socket.addEventListener('close', (event) => {
+    console.log(`connection closed code: ${event.code} reason: ${event.reason}`);
+});
 
-// socket.addEventListener('error', (error) => {
-// console.error('connection error:', error);
-// });
+socket.addEventListener('error', (error) => {
+    console.error('connection error:', error);
+});
